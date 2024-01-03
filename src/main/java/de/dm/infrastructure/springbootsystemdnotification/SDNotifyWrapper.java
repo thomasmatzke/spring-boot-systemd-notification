833c20b820fa8e18906b5a6c1756fc75ed389a09
MATZKE Thomas <matzke.thomas@gmail.com>
2024-01-03 21:09:42 +0100

    fix logging

--- src/main/java/de/dm/infrastructure/springbootsystemdnotification/SDNotifyWrapper.java
+++ src/main/java/de/dm/infrastructure/springbootsystemdnotification/SDNotifyWrapper.java
@@ -7,31 +7,31 @@ import info.faljse.SDNotify.SDNotify;
 
 public class SDNotifyWrapper implements SystemdNotifier {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(SDNotifyWrapper.class);
+   private static final Logger LOGGER = LoggerFactory.getLogger(SDNotifyWrapper.class);
 
-    @Override
-    public void sendMainPID() {
-        final int pid = Math.toIntExact(ProcessHandle.current().pid());
-        LOGGER.debug("Sending main PID to systemd: {}..." + pid);
-        SDNotify.sendMainPID(pid);
-    }
+   @Override
+   public void sendMainPID() {
+      final int pid = Math.toIntExact(ProcessHandle.current().pid());
+      LOGGER.debug("Sending main PID to systemd: {}...", pid);
+      SDNotify.sendMainPID(pid);
+   }
 
-    @Override
-    public void sendNotify() {
-        LOGGER.debug("Notifying systemd...");
-        SDNotify.sendNotify();
-    }
+   @Override
+   public void sendNotify() {
+      LOGGER.debug("Notifying systemd...");
+      SDNotify.sendNotify();
+   }
 
-    @Override
-    public void sendStatus(final String status) {
-        LOGGER.debug("Sending status to systemd: {}...", status);
-        SDNotify.sendStatus(status);
-    }
+   @Override
+   public void sendStatus(final String status) {
+      LOGGER.debug("Sending status to systemd: {}...", status);
+      SDNotify.sendStatus(status);
+   }
 
-    @Override
-    public void sendWatchdogKeepalive() {
-        LOGGER.debug("Sending watchdog keepalive message to systemd: {}...");
-        SDNotify.sendWatchdog();
-    }
+   @Override
+   public void sendWatchdogKeepalive() {
+      LOGGER.debug("Sending watchdog keepalive message to systemd...");
+      SDNotify.sendWatchdog();
+   }
 
 }
